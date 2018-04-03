0153ec504a9665602cfff1bc244f7623a27bf6d7
rita.zeng <rita.zeng@udacity.com>
2018-04-03 16:16:03 +0800

    delete duplication
    remove duplication

--- RL-Quadcopter-master/quad_controller_rl/src/quad_controller_rl/agents/__init__.py
+++
@@ -1,2 +0,0 @@
-from quad_controller_rl.agents.base_agent import BaseAgent
-from quad_controller_rl.agents.policy_search import RandomPolicySearch
--- RL-Quadcopter-master/quad_controller_rl/src/quad_controller_rl/tasks/__init__.py
+++
@@ -1,2 +0,0 @@
-from quad_controller_rl.tasks.base_task import BaseTask
-from quad_controller_rl.tasks.takeoff import Takeoff
